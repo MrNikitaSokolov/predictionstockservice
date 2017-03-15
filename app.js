3838aeffcc924fa2a71cb50334ec7ea4f5eca9ba
Nikita Sokolov <nsokolo@uwo.ca>
2017-03-15 10:28:54 -0400

    Status API

--- app.js
+++ app.js
@@ -26,3 +26,10 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+app.get('/status', function(req, res){
+    res.send({
+        name: 'Stock Market Prediction',
+        description: 'Stock market prediction is a service that help investor(s) to determine the future value of a company stock.'
+    });
+});
