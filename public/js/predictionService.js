9ba1618c557b023b4c4effb6de41143f240174a3
Jonathan Lam <jlam323@uwo.ca>
2017-04-06 06:01:48 -0400

    Working implementation using sample data

--- public/js/predictionService.js
+++ public/js/predictionService.js
@@ -1,20 +1,41 @@
-var time;
-var stock;
-
 var median;
 var mean;
+var time = 1/7;
+var predictClicked = false;
+
+
+function loadData(){
+    var select = document.getElementById('companyList');
+    var stock = select.options[select.selectedIndex].value;
+    var dataFile;
+
+    dataFile = "js/test-data/test-" + stock + ".js";
+
+    $.getScript(dataFile, function(){
+        predictStock();
+    });
+}
 
 function setTime(){
     var radio = document.getElementsByName('time');
 
     if (radio[0].checked)
+        //1/7 of a week
         time = 1/7;
     else if (radio[1].checked)
+        //1 week
         time = 1;
     else if (radio[2].checked)
+        //52 weeks
         time = 52;
     else {
         //error
+    }
+
+    //after the user has clicked predict stock, automatically call it when the time is changed
+    if (predictClicked){
+        //based on the stock chosen, load the data file
+        loadData();
     }
 }
 
@@ -24,19 +45,33 @@ function predictStock(){
     var value;
     var splitValue;
 
-    setTime();
+    predictClicked = true;
 
+    //go through the data file and get the accumulated value of the stock over time
     for (var i = 0; i < data.length; i++){
         value = data[i] + '';
         splitValue = value.split(',');
         total = total + parseInt(splitValue[1]);
     }
 
+    //calculate the average price
     mean = total / data.length;
 
+    //call function to find the median value
     findMedian();
 
-    var prediction = mean + ((mean - median) * time * 0.1);
+    //apply a change over time to the most recent stock price
+    var lastValue = parseInt(splitValue[1]);
+    var prediction = lastValue + ((mean - median) * time * 0.1);
+
+    //value cannot be negative
+    if (prediction < 0)
+        prediction = 0;
+
+    //round the number
+    prediction = Math.round(prediction * 100) / 100;
+
+    //call function to update the prediction value
     updatePrediction(prediction);
 }
 
@@ -49,7 +84,6 @@ function findMedian(){
     else
         middle = data.length / 2;
 
-
     var value;
     var splitValue;
 
@@ -60,5 +94,10 @@ function findMedian(){
 }
 
 function updatePrediction(prediction){
-    document.getElementById('prediction').innerHTML = prediction;
+    document.getElementById('prediction').innerHTML = "Projected Stock Value: $" + prediction;
+}
+
+function clearPrediction(){
+    document.getElementById('prediction').innerHTML = "Projected Stock Value: n/a"
+    predictClicked = false;
 }
