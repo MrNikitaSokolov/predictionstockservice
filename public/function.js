779ac7d1a6d5fe4dad404d85e67ab28d12452b9b
Jonathan Lam <jlam323@uwo.ca>
2017-04-06 03:15:06 -0400

    Move file

--- public/function.js
+++ public/function.js
@@ -4,7 +4,7 @@ $(function(){
         var company = $('#company-list').find(":selected").text();
 
         $.get('/getdata/' + company, function(dataset){
-
+            predictStock();
         }); //end of getdata
 
     });//end of select-company.click
