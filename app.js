d9f1bd069dbdfdc4aa9286abb30742e25788bdd8
Jonathan Lam <jlam323@uwo.ca>
2017-04-06 02:45:28 -0400

    Test connect

--- app.js
+++ app.js
@@ -12,8 +12,15 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+// for initiating HTTP request
+var rp = require('request-promise');
+
 // create a new express server
 var app = express();
+
+// use hbs as the view template
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -21,10 +28,17 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+var marketDataHost = 'https://stockmasterdata.mybluemix.net/company/';
+
+// ROUTES
+app.get('/', function(req, res){
+    rp({ uri: marketDataHost, json: true })
+        .then(function (data) {
+            res.render('index', {companyList: data});
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 });
 
 app.get('/status', function(req, res){
@@ -32,4 +46,22 @@ app.get('/status', function(req, res){
         name: 'Stock Market Prediction',
         description: 'Stock market prediction is a service that help investor(s) to determine the future value of a company stock.'
     });
+});
+
+
+app.get('/getdata/:companyName', function(req, res){
+    var company = req.params.companyName;
+    rp({ uri: marketDataHost + company, json: true })
+        .then(function (data) {
+            res.send(data);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+});
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+    // print a message when the server starts listening
+    console.log("server starting on " + appEnv.url);
 });
